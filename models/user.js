f1ce952dd783b4e7a85df5675eb4c5ca7d37b825
JonathanGFletcher <jonny.fletcher@icloud.com>
2021-04-11 15:03:36 -0600

    Strengthen User model

--- models/user.js
+++ models/user.js
@@ -1,24 +1,86 @@
-const mongoose =require("mongoose");
+const mongoose =require("mongoose"),
+{ Schema } = require("mongoose");
 
 
-userSchema = mongoose.Schema({
-    first: String,
-    last: String,
-    username: String,
-    gender: String,
-    street: String,
-    city: String,
-    state: String,
-    zipcode: String,
-    dob: Date,
-    email: String,
-    password: String,
-    sQuestion1: String,
-    sQuestion2: String,
-    sQuestion3: String,
-    sAnswer1: String,
-    sAnswer2: String,
-    sAnswer3: String
-});
+userSchema = new Schema(
+    {
+        name: {
+            first: {
+                type: String,
+                required: true
+            },
+            last: {
+                type: String,
+                required: true
+            }
+        },
+        account: {
+            username: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true
+            },
+            password: {
+                type: String,
+                required: true
+            }
+        },
+        gender: {
+            type: String,
+            required: true
+        },
+        address: {
+            street: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            state: {
+                type: String,
+                required: true
+            },
+            zipcode: {
+                type: Number,
+                required: true
+            }
+        },
+        dob: {
+            type: Date,
+            required: true
+        },
+        security: {
+            question1: {
+                type: String,
+                required: true
+            },
+            question2: {
+                type: String,
+                required: true
+            },
+            question3: {
+                type: String,
+                required: true
+            },
+            answer1: {
+                type: String,
+                required: true
+            },
+            answer2: {
+                type: String,
+                required: true
+            },
+            answer3: {
+                type: String,
+                required: true
+            }
+        }
+    }
+);
 
 module.exports = mongoose.model("User", userSchema);
